3b2767f446c5d6c387e89ebe45e19afe67151794
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-26 09:41:06 +0530

    Update vite.config.js

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://e-commerce-gclo.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api'),
       },
