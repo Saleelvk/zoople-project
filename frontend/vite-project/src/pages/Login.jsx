72a5845ddbcabef7758efe9912e88ab7348b204b
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-27 10:33:18 +0530

    Update Login.jsx

--- frontend/vite-project/src/pages/Login.jsx
+++ frontend/vite-project/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState ,useContext} from "react";
 import LoginImage from "../assets/image/bg-remove-image/computer-monitor.png";
 import { FaRegEye, FaRegEyeSlash } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
